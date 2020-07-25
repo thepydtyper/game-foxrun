08a804e0c26d9ba7a7c43e5561bebea87ab81946
Steveo Weevo <pydtyper@gmail.com>
2020-07-24 21:02:24 -0400

    added bat enemy script

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,21 +2,25 @@ extends KinematicBody2D
 
 const ACCELERATION = 500
 const MAX_SPEED = 80
+const ROLL_SPEED = 120
 const FRICTION = 500
 
 enum {MOVE, ROLL, ATTACK}
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 
 func _ready():
 	
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 
 func _physics_process(delta):
@@ -25,7 +29,7 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
 
@@ -38,6 +42,8 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
@@ -48,7 +54,10 @@ func move_state(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
@@ -56,16 +65,25 @@ func move_state(delta):
 
 func roll_state(delta):
 	
-	pass
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
 
 func attack_state(delta):
 	
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+
+
+func move():
 	
-	
+	velocity = move_and_slide(velocity)
+
+
 func roll_animation_finished():
 	
+	velocity = Vector2.ZERO
 	state = MOVE
 	
 	
