3f67bf8ea2531d03033cf9f1fcc4726ecd04ac01
Steveo Weevo <pydtyper@gmail.com>
2020-07-25 21:22:33 -0400

    added bat AI

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,9 +2,19 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
-var knockback = Vector2.ZERO
+enum {IDLE, WANDER, CHASE}
 
+var knockback = Vector2.ZERO
+var state = CHASE
+var velocity = Vector2.ZERO
+
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerdetectionzone = $PlayerDetectionZone
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
 
 func _ready():
 	
@@ -16,6 +26,28 @@ func _physics_process(delta):
 	
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, 200 * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = playerdetectionzone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				sprite.flip_h = velocity.x < 0
+			else:
+				state = IDLE
+	velocity = move_and_slide(velocity)
+
+
+func seek_player():
+	
+	if playerdetectionzone.can_see_player():
+		state = CHASE
 
 
 func _on_Hurtbox_area_entered(area):
